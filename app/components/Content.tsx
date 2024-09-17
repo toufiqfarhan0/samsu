18ec66b4b6c870f2da875fe4f64f3f5f04ea9237
Farhan <144435177+toufiqfarhan0@users.noreply.github.com>
2024-09-17 23:23:26 +0530

    small ui changes2

--- app/components/Content.tsx
+++ app/components/Content.tsx
@@ -3,11 +3,13 @@ import { InfiniteMovingCardsDemo } from "./InfinieCards";
 
 const Content = () => {
   return (
-    <div className="flex justify-center items-center">
-      <div className="flex justify-center items-center">
-        <h1 className="text-4xl font-semibold">What do we offer?</h1>
+    <div className="flex flex-col justify-center items-center px-4 py-6 md:py-12">
+      <div className="mb-6 text-center">
+        <h1 className="text-3xl md:text-4xl font-semibold">
+          What do we offer?
+        </h1>
       </div>
-      <div className="flex items-center justify-center">
+      <div className="w-full flex items-center justify-center">
         <InfiniteMovingCardsDemo />
       </div>
     </div>
